2bbdbd9ee0a3a59376e43a8b234e4c64bca9cae3
Vadim <botgugla@gmail.com>
2024-07-22 19:07:27 +0300

    User model changes

--- src/main/java/org/roadmap/weatherapp/model/User.java
+++ src/main/java/org/roadmap/weatherapp/model/User.java
@@ -1,14 +1,21 @@
 package org.roadmap.weatherapp.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -16,16 +23,24 @@ import lombok.Setter;
 @NoArgsConstructor
 @Table(name = "users")
 public class User extends Model {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+    @ColumnDefault("nextval('user_id_seq'")
+    @Column(name = "id", nullable = false, unique = true)
     private Integer id;
 
-    @Column(name = "login", length = Integer.MAX_VALUE)
+    @Column(name = "login", length = Integer.MAX_VALUE, unique = true)
     private String login;
 
     @Column(name = "password", length = Integer.MAX_VALUE)
     private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    private List<Location> locations = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    private List<UserSession> sessions = new ArrayList<>();
 
     public User(String login, String password) {
         this.login = login;
